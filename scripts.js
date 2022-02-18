14ac37af4a68a9934e73ac629b6ab15a43d6294e
aaustin101 <82051339+aaustin101@users.noreply.github.com>
2022-02-18 10:09:05 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $("#response").html("scripts.js has loaded!");
 
 let userInput;
-userInput = "samosa";
+userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
   $("#response").html("Brilliant choice!");
 } else {
