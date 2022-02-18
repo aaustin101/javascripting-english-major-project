e17c35c910b623a59595da01e517dcca079282d3
aaustin101 <82051339+aaustin101@users.noreply.github.com>
2022-02-18 09:55:59 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 alert("scripts.js has loaded!");
+
+let userInput;
+userInput = "samosa";
+if ( userInput === "burrito" ) {
+    console.log("Brilliant choice!");
+  } else {
+    console.log("Don’t you want a burrito?");
+  }
