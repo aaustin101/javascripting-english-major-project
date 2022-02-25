eda3e342cc30282d0dd21b5f1a230e31b5ec8542
aaustin101 <82051339+aaustin101@users.noreply.github.com>
2022-02-25 09:27:42 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 $("#response").html("scripts.js has loaded!");
 
-let userInput;
+/*let userInput;
 userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
   $("#response").html("Brilliant choice!");
 } else {
   $("#response").html("Don’t you want a burrito?");
 }
+*/
+
+let userInput;
+userInput = prompt("Is the weather nice outside today", "Type your answer here");
+if ( userInput === "no" {
+    $(#response").html("correct the weather is very bad!");
+    } else {
+  $(#response").html("YOU ARE WRONG!!!!! The weather is very bad!");
+    }
