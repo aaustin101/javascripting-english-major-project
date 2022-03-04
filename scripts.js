73851fbdd9bb3e918e30f0e8e37234797040495a
aaustin101 <82051339+aaustin101@users.noreply.github.com>
2022-03-04 09:41:58 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,7 +32,7 @@ whiteonion = "no opinion';
 makeABurrito(whiteonion);
 */
 
-
+/*
 let arrayOfStrings, arrayOfNumbers, arrayMixed;
 arrayOfStrings = ["a", "b", "c"];
 arrayOfNumbers = [1, 2, 3];
@@ -53,3 +53,29 @@ $("#response").html(myBurritoObject["tortilla"]);
 let arrayOfStrings;
 arrayOfStrings = ["a", "b", "c"];
 $("#response").html(arrayOfStrings.length);
+*/
+
+let myHabaneroSauceSquirts, myBurritoObject;
+// First, define and assign a variable for how 
+// spicy the burrito is.
+myHabaneroSauceSquirts = 3;
+// Now assign the burrito object.
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  // Make use of the variable above.
+  habaneroSauceSquirts: myHabaneroSauceSquirts,
+  // Use the variable again in a function.
+  spiciness: function(){
+    if (myHabaneroSauceSquirts > 0 ){
+      alert("This is a spicy burrito!");
+    } else {
+      alert("This is a mild burrito.");
+    }
+  }
+};
+$("#response").html("Your burrito has " +
+  myBurritoObject.habaneroSauceSquirts +
+  " squirts of habanero.");
+myBurritoObject.spiciness();
