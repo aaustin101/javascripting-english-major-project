ccce484304b95893a331f2d669a0ad5d6f94473e
aaustin101 <82051339+aaustin101@users.noreply.github.com>
2022-03-04 09:40:54 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -38,3 +38,18 @@ arrayOfStrings = ["a", "b", "c"];
 arrayOfNumbers = [1, 2, 3];
 arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
 $("#response").html(arrayOfStrings[2]);
+
+
+let myBurritoObject;
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  habaneroSauceSquirts: 3
+  };
+// and let’s access a property
+$("#response").html(myBurritoObject["tortilla"]);
+
+let arrayOfStrings;
+arrayOfStrings = ["a", "b", "c"];
+$("#response").html(arrayOfStrings.length);
