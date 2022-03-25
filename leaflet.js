c6c2b5eaf4b96767486cf8baccd5693081e7eeec
aaustin101 <82051339+aaustin101@users.noreply.github.com>
2022-03-25 10:25:35 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -9,42 +9,23 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 tileLayer.addTo(firstMap);
 firstMap.setView([40.730833, -73.9975], 16);
 
-let bobstLibrary;
-bobstLibrary = L.marker([40.729444, -73.997222]);
-bobstLibrary.addTo(firstMap);
-bobstLibrary.bindPopup("This is Bobst Library.");
-
-let bobstCoords;
-bobstCoords = L.latLng(40.729444, -73.997222);
-firstMap.panTo(bobstCoords);
-
-let eiffelTower;
-> eiffelTower = L.latLng(48.858222, 2.2945);
-> firstMap.panTo(eiffelTower);
-
-let washingtonSquarePark, washingtonSquareParkMarker;
-let tenThousandth, tenThousandthPolygon, thousandth, thousandthPolyline;
+let washingtonSquarePark, washingtonSquareParkMarker, bobstLibrary, bobstLibraryMarker, waverlyPlace, waverlyPlaceMarker;
 washingtonSquarePark = L.latLng(40.730833, -73.9975);
-washingtonSquareParkMarker = L.marker(washingtonSquarePark).addTo(firstMap);
-tenThousandth = [[40.7307, -73.9976], [40.7307, -73.9974], 
-                 [40.7309, -73.9974], [40.7309, -73.9976]];
-tenThousandthPolygon = L.polygon(tenThousandth, {
-                                  color: "#268bd2",
-                                  fillColor: "#fdf6e3"
-                                }
-                        ).addTo(firstMap);
-
-thousandth = [[washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng + 0.001],
-              [washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng - 0.001],
-              [washingtonSquarePark.lat - 0.001, washingtonSquarePark.lng - 0.001],
-              [washingtonSquarePark.lat - 0.001, washingtonSquarePark.lng + 0.001]];
-thousandthPolyline = L.polyline(thousandth, {color: "#d33682"}
-                      ).addTo(firstMap);
-
+washingtonSquareParkMarker = L.marker(washingtonSquarePark).bindPopup("This is Washington Square Park.").addTo(firstMap);
+bobstLibrary = L.latLng(40.729444, -73.997222);
+bobstLibraryMarker = L.marker(bobstLibrary).bindPopup("This is Bobst Library.").addTo(firstMap);
+waverlyPlace = L.latLng(40.7344954, -74.0035869);
+waverlyPlaceMarker = L.marker(waverlyPlace).bindPopup("I added another point, fools.").addTo(firstMap);
 let circle;
 circle = L.circle(washingtonSquarePark, {radius: 100,
-                                         color: "#859900",
-                                         fillColor: "#cb4b16",
+                                         color: "EFBB24",
+                                         fillColor: "EFBB24",
                                          opacity: 0.9,
-                                         fillOpacity: 0.25}
+                                         fillOpacity: 0.20}
+          ).addTo(firstMap);
+circle = L.circle(bobstLibrary,          {radius: 100,
+                                          color: "EFBB24",
+                                          fillColor: "EFBB24",
+                                          opacity: 0.9,
+                                          fillOpacity: 0.20}
           ).addTo(firstMap);
