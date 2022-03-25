a9359fdfc9c484f81930256653d5ff41735a3f3c
aaustin101 <82051339+aaustin101@users.noreply.github.com>
2022-03-25 10:24:14 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -33,3 +33,18 @@ tenThousandthPolygon = L.polygon(tenThousandth, {
                                   fillColor: "#fdf6e3"
                                 }
                         ).addTo(firstMap);
+
+thousandth = [[washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng + 0.001],
+              [washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng - 0.001],
+              [washingtonSquarePark.lat - 0.001, washingtonSquarePark.lng - 0.001],
+              [washingtonSquarePark.lat - 0.001, washingtonSquarePark.lng + 0.001]];
+thousandthPolyline = L.polyline(thousandth, {color: "#d33682"}
+                      ).addTo(firstMap);
+
+let circle;
+circle = L.circle(washingtonSquarePark, {radius: 100,
+                                         color: "#859900",
+                                         fillColor: "#cb4b16",
+                                         opacity: 0.9,
+                                         fillOpacity: 0.25}
+          ).addTo(firstMap);
