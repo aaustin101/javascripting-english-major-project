bb76931eeee510f8a19453d8224f9ce0cc864afb
aaustin101 <82051339+aaustin101@users.noreply.github.com>
2022-05-10 16:16:26 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -9,29 +9,31 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 tileLayer.addTo(firstMap);
 firstMap.setView([40.730833, -73.9975], 16);
 
-let washingtonSquarePark, washingtonSquareParkMarker, bobstLibrary, bobstLibraryMarker, waverlyPlace, waverlyPlaceMarker;
-washingtonSquarePark = L.latLng(40.730833, -73.9975);
-washingtonSquareParkMarker = L.marker(washingtonSquarePark).bindPopup("This is Washington Square Park.").addTo(firstMap);
-bobstLibrary = L.latLng(40.729444, -73.997222);
-bobstLibraryMarker = L.marker(bobstLibrary).bindPopup("This is Bobst Library.").addTo(firstMap);
-waverlyPlace = L.latLng(40.73449, -74.00358);
-waverlyPlaceMarker = L.marker(waverlyPlace).bindPopup("I added another point, fools.").addTo(firstMap);
+let dashop, dashopMarker, midtownComics, midtownComicsMarker, toyTokyo, toyTokyoMarker;
+dashop = L.latLng(40.730833, -73.9975);
+dashopMarker = L.marker(dashop).bindPopup("This is Washington Square Park.").addTo(firstMap);
+midtownComics = L.latLng(40.729444, -73.997222);
+midtownComicsMarker = L.marker(midtownComics).bindPopup("This is Bobst Library.").addTo(firstMap);
+toyTokyo = L.latLng(40.73449, -74.00358);
+toyTokyoMarker = L.marker(toyTokyo).bindPopup("I added another point, fools.").addTo(firstMap);
 let circle;
-circle = L.circle(washingtonSquarePark, {radius: 100,
+circle = L.circle(dashop, {radius: 100,
                                          color: "#ffccff",
                                          fillColor: "#ffccff",
                                          opacity: 0.9,
                                          fillOpacity: 0.20}
           ).addTo(firstMap);
-circle = L.circle(bobstLibrary,          {radius: 100,
+circle = L.circle(midtownComics,          {radius: 100,
                                           color: "#ffccff",
                                           fillColor: "#ffccff",
                                           opacity: 0.9,
                                           fillOpacity: 0.20}
           ).addTo(firstMap);
-circle = L.circle(waverlyPlace,          {radius: 100,
+circle = L.circle(toyTokyo,          {radius: 100,
                                           color: "#d33682",
                                           fillColor: "#d33682",
                                           opacity: 0.9,
                                           fillOpacity: 0.20}
           ).addTo(firstMap);
+
+
